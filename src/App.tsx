b49142e4c17df3585197e67a1f3d1a20f1efd983
Dinesh-9025 <dineshcareer4@gmail.com>
2022-12-13 11:06:53 +0530

    added changess

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import './App.css';
 
 function App() {
 
-  const [autoPlay, setAutoPlay] = useState<boolean>(true)
+  const autoPlay: any = true
 
 
   async function StartStreaming() {
     var video: any = document.querySelector("#videoElement")
     var audio: any = document.querySelector("#videoElement")
 
-    if (navigator.mediaDevices.getUserMedia ) {
+    if (navigator.mediaDevices.getUserMedia) {
       navigator.mediaDevices.getUserMedia({ video: true, audio: true })
         .then(function (stream) {
           console.log("stream", stream);
