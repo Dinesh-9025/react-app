743a53ab8e4566cc091d8ac57251d6b0df244720
Dinesh-9025 <dineshcareer4@gmail.com>
2022-12-19 12:38:37 +0530

    added changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,35 @@ import './App.css';
 
 function App() {
 
-  const autoPlay: any = true
+  const autoPlay: boolean = true
 
 
   async function StartStreaming() {
     var video: any = document.querySelector("#videoElement")
     var audio: any = document.querySelector("#videoElement")
 
+    navigator.mediaDevices.enumerateDevices()
+
     if (navigator.mediaDevices.getUserMedia) {
+
       navigator.mediaDevices.getUserMedia({ video: true, audio: true })
         .then(function (stream) {
           console.log("stream", stream);
           sessionStorage.setItem("id", stream.id)
           video.srcObject = stream;
           audio.srcObject = stream;
+          video.onloadedmetadata = () => {
+            video.play();
+          };
         })
         .catch(function (err0r) {
-          console.log("Something went wrong!");
+          console.log("Something went wrong!", err0r);
         });
     }
   }
 
   // function stop(e: any) {
-  //   console.log("e", e) 
+  //   console.log("e", e)
 
   // }
 
@@ -34,8 +40,8 @@ function App() {
 
       <div className='body'>
         <video autoPlay={autoPlay} id="videoElement">
-          <source src="x" type="video/x" />
-          <source src="x" type="x" />
+          {/* <source src="x" type="video/x" />
+          <source src="x" type="x" /> */}
         </video>
       </div>
       <div>
