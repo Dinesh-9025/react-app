2a419971132d68cfb25713429756cc83041a4b48
Dinesh-9025 <dineshcareer4@gmail.com>
2022-12-19 13:52:36 +0530

    added changes

--- src/fireBase.tsx
+++ src/fireBase.tsx
@@ -4,14 +4,14 @@ import { getAnalytics } from "firebase/analytics";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDbD5pfc25AEBZJ_w6Emk_muIDjKvZ9Elk",
-    authDomain: "web-cam-41322.firebaseapp.com",
-    databaseURL: "https://web-cam-41322-default-rtdb.firebaseio.com",
-    projectId: "web-cam-41322",
-    storageBucket: "web-cam-41322.appspot.com",
-    messagingSenderId: "463136215985",
-    appId: "1:463136215985:web:8fbb540bf4e82527392390",
-    measurementId: "G-3XZLY95YYK"
+  apiKey: "AIzaSyDbD5pfc25AEBZJ_w6Emk_muIDjKvZ9Elk",
+  authDomain: "web-cam-41322.firebaseapp.com",
+  databaseURL: "https://web-cam-41322-default-rtdb.firebaseio.com",
+  projectId: "web-cam-41322",
+  storageBucket: "web-cam-41322.appspot.com",
+  messagingSenderId: "463136215985",
+  appId: "1:463136215985:web:8fbb540bf4e82527392390",
+  measurementId: "G-3XZLY95YYK"
 };
 
 
